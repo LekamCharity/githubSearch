ac882977b10148059440672801c0162b29876f67
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-05 10:34:33 +0300

    added new files

--- src/app/http-service.ts
+++ src/app/http-service.ts
@@ -1,2 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+ providedIn: 'root'
+})
 export class HttpService {
+ 
+  repos$ //repos object
+
+  constructor(private http:HttpClient) {
+   }
+
+  userRequested(User){
+
+    interface expected{
+      avatar_url:any,
+      login:string,
+      email:string,
+      location:string,
+      created_at:Date,
+      public_repos:number,
+      followers:number,
+      following:number,
+      html_url:string
+
+    }
+}
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+
 })
 export class ProfileComponent implements OnInit {
 
