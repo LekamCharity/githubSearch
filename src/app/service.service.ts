2a112ed05725adde46fa974c7d5b76f66dedbd54
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-05 12:04:15 +0300

    called for a promise

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -11,11 +11,39 @@ import { allowedNodeEnvironmentFlags } from 'process';
 export class ServiceService {
   user: User;
   repo: Repo;
-  
+
   profile = environment.url;
 
-  constructor(private http:HttpClient) { 
-    this.user = new User('', '', '','', 0, 0, 0, 0, '')
-    this.repo = new Repo( '', 0, 0)
+  constructor(private http: HttpClient) {
+    this.user = new User('', '', '', '', 0, 0, 0, 0, '')
+    this.repo = new Repo('', 0, 0)
+  }
+  userProfile(num: string) {
+    interface expected {
+      login: string,
+      avatar_url: string,
+      email: string,
+      location: string,
+      memberSince: Number,
+      repos: number,
+      followers: number,
+      following: number,
+      url: string
+    }
+    let promise = new Promise((resolve, reject)) => {
+      this.http.get<expected>('https://api.github.com/users/' + num + this.profile).toPromise().then(data => {
+        this.user = new User(
+          data.login,
+          data.avatar_url,
+          data.email,
+          data.location,
+          data.memberSince,
+          data.repos,
+          data.followers,
+          data.following,
+          data.url
+        )
+      })
+    }
   }
 }
